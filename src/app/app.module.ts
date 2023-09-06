87877108219ab13ccb880486505f2e1201b872fe
Gerard Solanes <gerardsolher@gmail.com>
2023-09-06 13:17:17 +0200

    Clean up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TarjetasComponent } from './tarjetas/tarjetas.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TarjetasComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/tarjetas/tarjetas.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tarjetas',
+  templateUrl: './tarjetas.component.html',
+  styleUrls: ['./tarjetas.component.scss']
+})
+export class TarjetasComponent {
+
+}
