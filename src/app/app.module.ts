831ab77ce56e4b4a83e6df129dfacd4a9fbd70aa
Gerard Solanes <gerardsolher@gmail.com>
2023-09-06 16:15:23 +0200

    Generates navbar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TarjetasComponent } from './tarjetas/tarjetas.component';
 import { IconComponent } from './icon/icon.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TarjetasComponent,
-    IconComponent
+    IconComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
