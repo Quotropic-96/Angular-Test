96a7c10ce6f385eba2cadd53f44a039ddace7726
Gerard Solanes <gerardsolher@gmail.com>
2023-09-06 15:48:41 +0200

    Creates icon component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TarjetasComponent } from './tarjetas/tarjetas.component';
+import { IconComponent } from './icon/icon.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TarjetasComponent
+    TarjetasComponent,
+    IconComponent
   ],
   imports: [
     BrowserModule,
