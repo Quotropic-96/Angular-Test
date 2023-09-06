34e0bebd0521bc70699b3b6f8a87f55410efc570
Gerard Solanes <gerardsolher@gmail.com>
2023-09-06 16:56:51 +0200

    Adds route for TarjetasComponent

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TarjetasComponent } from './tarjetas/tarjetas.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: TarjetasComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
