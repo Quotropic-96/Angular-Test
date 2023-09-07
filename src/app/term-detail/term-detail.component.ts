5bc902f635d4f870001bea042641b42c0131b96b
Gerard Solanes <gerardsolher@gmail.com>
2023-09-07 11:58:28 +0200

    Adds term-detail component content

--- src/app/term-detail/term-detail.component.ts
+++ src/app/term-detail/term-detail.component.ts
@@ -8,6 +8,59 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class TermDetailComponent implements OnInit {
   termId: string = '';
+  termNumber: number = 0;
+  completedSessions: number = 0;
+  totalSessions: number = 0;
+  sessions = [
+    {
+      sessionNumber: 22,
+      sessionTitle: 'Ordinales',
+      sessionState: 'Done',
+      isNext: false
+    },
+    {
+      sessionNumber: 23,
+      sessionTitle: 'Ordinales y fracciones',
+      sessionState: 'Done',
+      isNext: false
+    },
+    {
+      sessionNumber: 24,
+      sessionTitle: 'Fracciones y decimales',
+      sessionState: 'Done',
+      isNext: false
+    },
+    {
+      sessionNumber: 25,
+      sessionTitle: 'Ordinales, fracciones y decimales',
+      sessionState: 'Pending',
+      isNext: true
+    },
+    {
+      sessionNumber: 26,
+      sessionTitle: 'Ordinales, fracciones, decimales y geometría',
+      sessionState: 'Pending',
+      isNext: false
+    },
+    {
+      sessionNumber: 27,
+      sessionTitle: 'Geometría y fracciones',
+      sessionState: 'Pending',
+      isNext: false
+    },
+    {
+      sessionNumber: 28,
+      sessionTitle: 'Fracciones, decimales y gemoetría',
+      sessionState: 'Pending',
+      isNext: false
+    },
+    {
+      sessionNumber: 29,
+      sessionTitle: 'Ordinales, Fracciones, Decimales, Gemoetría, Numeros Enteros',
+      sessionState: 'Pending',
+      isNext: false
+    }
+  ]
 
   constructor(private route: ActivatedRoute) {}
 
@@ -15,7 +68,9 @@ export class TermDetailComponent implements OnInit {
     this.route.params.subscribe((params) => {
       this.termId = params['id'];
       // Fetch term info from db
-      
+      this.termNumber = parseInt(this.termId);
+      this.completedSessions = 20;
+      this.totalSessions = 50;
     });
   }
 }
