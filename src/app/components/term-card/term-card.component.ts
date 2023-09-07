75d87d14fdee8ceb74f3271986d0a36081299cf1
Gerard Solanes <gerardsolher@gmail.com>
2023-09-07 17:12:11 +0200

    Refactors term card component for progress bar

--- src/app/components/term-card/term-card.component.ts
+++ src/app/components/term-card/term-card.component.ts
@@ -12,6 +12,14 @@ export class TermCardComponent {
 
   isFullCircle: boolean = false;
 
+  graphSize: number = 60;
+  iconSize: number = 50;
+  radius: number = this.graphSize / 2;
+  centerX: number = this.graphSize / 2;
+  centerY: number = this.graphSize / 2;
+  iconX: number = (this.graphSize - this.iconSize) / 2;
+  iconY: number = (this.graphSize - this.iconSize) / 2;
+
   ngOnInit() {
     this.isFullCircle = this.completedSessions === this.totalSessions;
   }
@@ -21,25 +29,22 @@ export class TermCardComponent {
       return '';
     }
 
-    const radius = 40;
-    const centerX = 50;
-    const centerY = 50;
-    const startAngle = 0;
-    const endAngle = 360 * this.completedSessions / this.totalSessions;
+    const startAngle = -90;
+    const endAngle = startAngle + 360 * this.completedSessions / this.totalSessions;
 
     // Calculate the coordinates of the arc's start and end points
-    const startX = centerX + radius * Math.cos((startAngle * Math.PI) / 180);
-    const startY = centerY + radius * Math.sin((startAngle * Math.PI) / 180);
-    const endX = centerX + radius * Math.cos((endAngle * Math.PI) / 180);
-    const endY = centerY + radius * Math.sin((endAngle * Math.PI) / 180);
+    const startX = this.centerX + this.radius * Math.cos((startAngle * Math.PI) / 180);
+    const startY = this.centerY + this.radius * Math.sin((startAngle * Math.PI) / 180);
+    const endX = this.centerX + this.radius * Math.cos((endAngle * Math.PI) / 180);
+    const endY = this.centerY + this.radius * Math.sin((endAngle * Math.PI) / 180);
 
     // Construct the path string for the filled portion of the circle
     const largeArcFlag = endAngle - startAngle <= 180 ? '0' : '1';
     const path = [
-      `M ${startX} ${startY}`, // Move to the start point
-      `A ${radius} ${radius} 0 ${largeArcFlag} 1 ${endX} ${endY}`, // Draw an arc (clockwise)
-      `L ${centerX} ${centerY}`, // Draw a line to the center
-      'Z' // Close the path to create a filled sector
+      `M ${startX} ${startY}`,
+      `A ${this.radius} ${this.radius} 0 ${largeArcFlag} 1 ${endX} ${endY}`,
+      `L ${this.centerX} ${this.centerY}`,
+      'Z' 
     ].join(' ');
 
     return path;
