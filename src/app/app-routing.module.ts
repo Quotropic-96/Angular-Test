629f227bac69a7f2a896d33abcefbb656e5cdc97
Gerard Solanes <gerardsolher@gmail.com>
2023-09-08 11:11:02 +0200

    Styles before start view

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './views/home/home.component';
 import { TermDetailComponent } from './views/term-detail/term-detail.component';
+import { BeforeStartComponent } from './views/before-start/before-start.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'term-detail/:id', component: TermDetailComponent },
+  { path: 'start-session/:id', component: BeforeStartComponent}
 ];
 
 @NgModule({
