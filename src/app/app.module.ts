478a3d857fbea921819c96b3289dce7868e589ed
Gerard Solanes <gerardsolher@gmail.com>
2023-09-08 10:37:44 +0200

    Implements app button component in home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { NavbarComponent } from './components/navbar/navbar.component';
 import { TermCardComponent } from './components/term-card/term-card.component';
 import { TermDetailComponent } from './views/term-detail/term-detail.component';
 import { SessionCardComponent } from './components/session-card/session-card.component';
+import { ButtonComponent } from './components/button/button.component';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import { SessionCardComponent } from './components/session-card/session-card.com
     NavbarComponent,
     TermCardComponent,
     TermDetailComponent,
-    SessionCardComponent
+    SessionCardComponent,
+    ButtonComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/button/button.component.ts
@@ -0,0 +1,28 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-button',
+  templateUrl: './button.component.html',
+  styleUrls: ['./button.component.scss']
+})
+export class ButtonComponent {
+  @Input() isPrimary: boolean = false;
+  @Input() isActive: boolean = false;
+  @Input() isIcon: boolean = false;
+  @Input() iconName: string = '';
+  @Input() buttonText: string = '';
+  @Input() rawToUrl: string = '';
+
+  toUrl: string[] = [''];
+
+  ngOnInit() {
+    const splitUrl = this.rawToUrl.split(':');
+    if (this.rawToUrl.length === 0) {
+      this.toUrl = ['#'];
+    } else if (splitUrl.length > 1) {
+      this.toUrl = splitUrl;
+    } else {
+      this.toUrl.push(this.rawToUrl);
+    }
+  }
+}
