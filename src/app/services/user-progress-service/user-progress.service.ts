af0715c404e76df238772d50f2fe664d5c0ebbb9
Gerard Solanes <gerardsolher@gmail.com>
2023-09-09 15:02:59 +0200

    Implements retrieveing info from mock api at term-detail view

--- src/app/services/user-progress-service/user-progress.service.ts
+++ src/app/services/user-progress-service/user-progress.service.ts
@@ -6,6 +6,7 @@ import { Term } from 'src/app/models/term.interface';
 import { Session } from 'src/app/models/session.interface';
 import { CourseId } from 'src/app/shared/courseId';
 import { UserTerm } from 'src/app/models/userTerm';
+import { UserSession } from 'src/app/models/userSession.interface';
 
 @Injectable({
   providedIn: 'root',
@@ -35,19 +36,50 @@ export class UserProgressService {
       totalSessions: 0,
       completedSessions: 0,
       isCompleted: false,
-    }
+    },
   ];
 
   constructor() {}
 
   getTermProgress(courseId: CourseId): UserTerm[] {
     return this.termProgress.map((term, idx) => {
-      let sessions = this.user.sessionProgress.filter(session => session.courseId === courseId && session.term === idx + 1)
+      let sessions = this.user.sessionProgress.filter(
+        (session) => session.courseId === courseId && session.term === idx + 1
+      );
       term.courseId = courseId;
       term.totalSessions = sessions.length;
-      term.completedSessions = sessions.filter(session => session.completed).length;
+      term.completedSessions = sessions.filter(
+        (session) => session.completed
+      ).length;
       term.isCompleted = term.totalSessions === term.completedSessions;
       return term;
-    })
+    });
+  }
+
+  getSingleTermProgress(courseId: CourseId, termNumber: number): UserTerm {
+    const selectedTerm = this.getTermProgress(courseId).filter(
+      (term) => term.termNumber === termNumber
+    );
+    if (selectedTerm.length != 0) {
+      return selectedTerm[0];
+    } else {
+      //Throw error
+    }
+    return selectedTerm[0];
+  }
+
+  getSessionsProgress(courseId: CourseId, termNumber: number): UserSession[] {
+    const filtteredSessions = this.user.sessionProgress.filter(
+      (session) => session.courseId === courseId && session.term === termNumber
+    );
+    return filtteredSessions.map((session, idx) => {
+      let userSession: UserSession = {
+        sessionNumber: session.sessionNumber,
+        sessionTitle: session.sessionId,
+        isDone: session.completed,
+        isNext: !session.completed && (idx === 0 ||filtteredSessions[idx - 1].completed),
+      };
+      return userSession;
+    });
   }
 }
--- src/app/views/term-detail/term-detail.component.ts
+++ src/app/views/term-detail/term-detail.component.ts
@@ -2,6 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
 import { UserTerm } from 'src/app/models/userTerm';
+import { UserProgressService } from 'src/app/services/user-progress-service/user-progress.service';
+import { SettingsService } from 'src/app/services/settings-service/settings.service';
+import { CourseId } from 'src/app/shared/courseId';
+import { UserSession } from 'src/app/models/userSession.interface';
 
 @Component({
   selector: 'app-term-detail',
@@ -9,77 +13,30 @@ import { UserTerm } from 'src/app/models/userTerm';
   styleUrls: ['./term-detail.component.scss'],
 })
 export class TermDetailComponent implements OnInit {
+  courseId: CourseId = '3i';
+  termId: number = 0;
   term: UserTerm = {
-      courseId: '3i',
-      termNumber: 1,
-      totalSessions: 0,
-      completedSessions: 0,
-      isCompleted: false
-  }
-  termId: string = '';
-  termNumber: number = 0;
-  completedSessions: number = 0;
-  totalSessions: number = 0;
-  sessions = [
-    {
-      sessionNumber: 22,
-      sessionTitle: 'Ordinales',
-      isDone: true,
-      isNext: false
-    },
-    {
-      sessionNumber: 23,
-      sessionTitle: 'Ordinales y fracciones',
-      isDone: true,
-      isNext: false
-    },
-    {
-      sessionNumber: 24,
-      sessionTitle: 'Fracciones y decimales',
-      isDone: true,
-      isNext: false
-    },
-    {
-      sessionNumber: 25,
-      sessionTitle: 'Ordinales, fracciones y decimales',
-      isDone: false,
-      isNext: true
-    },
-    {
-      sessionNumber: 26,
-      sessionTitle: 'Ordinales, fracciones, decimales y geometría',
-      isDone: false,
-      isNext: false
-    },
-    {
-      sessionNumber: 27,
-      sessionTitle: 'Geometría y fracciones',
-      isDone: false,
-      isNext: false
-    },
-    {
-      sessionNumber: 28,
-      sessionTitle: 'Fracciones, decimales y gemoetría',
-      isDone: false,
-      isNext: false
-    },
-    {
-      sessionNumber: 29,
-      sessionTitle: 'Ordinales, Fracciones, Decimales, Gemoetría, Numeros Enteros',
-      isDone: false,
-      isNext: false
-    }
-  ]
+    courseId: '3i',
+    termNumber: this.termId,
+    totalSessions: 0,
+    completedSessions: 0,
+    isCompleted: false,
+  };
+  sessions: UserSession[] = [];
 
-  constructor(private route: ActivatedRoute, private location: Location) {}
+  constructor(
+    private route: ActivatedRoute,
+    private location: Location,
+    private settingsService: SettingsService,
+    private userProgressService: UserProgressService
+  ) {}
 
   ngOnInit(): void {
     this.route.params.subscribe((params) => {
-      this.termId = params['id'];
-      // Fetch term info from db
-      this.termNumber = parseInt(this.termId);
-      this.completedSessions = 20;
-      this.totalSessions = 50;
+      this.courseId = this.settingsService.getCourse();
+      this.termId = parseInt(params['id']);
+      this.term = this.userProgressService.getSingleTermProgress(this.courseId, this.termId);
+      this.sessions = this.userProgressService.getSessionsProgress(this.courseId, this.termId);
     });
   }
 
