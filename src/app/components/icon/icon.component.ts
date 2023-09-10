2b85896fa8515981e32d636885301d8bf1068cd4
Gerard Solanes <gerardsolher@gmail.com>
2023-09-10 11:44:50 +0200

    Refactors menu styles

--- src/app/components/icon/icon.component.ts
+++ src/app/components/icon/icon.component.ts
@@ -3,8 +3,10 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'app-icon',
   templateUrl: './icon.component.html',
-  styleUrls: ['./icon.component.scss'],
+  styleUrls: [],
 })
 export class IconComponent {
   @Input() iconName: string = '';
+
+  constructor() {}
 }
