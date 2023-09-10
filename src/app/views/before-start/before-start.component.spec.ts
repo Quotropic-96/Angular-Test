7c4f0610b414b2044076c791afd9da1072dbc6b7
Gerard Solanes <gerardsolher@gmail.com>
2023-09-10 17:46:47 +0200

    Adds tests for befora start component

--- src/app/views/before-start/before-start.component.spec.ts
+++ src/app/views/before-start/before-start.component.spec.ts
@@ -1,21 +1,35 @@
-// import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { BeforeStartComponent } from './before-start.component';
+import { IconComponent } from 'src/app/components/icon/icon.component';
+import { ButtonComponent } from 'src/app/components/button/button.component';
 
-// import { BeforeStartComponent } from './before-start.component';
+describe('BeforeStartComponent', () => {
+  let component: BeforeStartComponent;
+  let fixture: ComponentFixture<BeforeStartComponent>;
 
-// describe('BeforeStartComponent', () => {
-//   let component: BeforeStartComponent;
-//   let fixture: ComponentFixture<BeforeStartComponent>;
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [BeforeStartComponent, IconComponent, ButtonComponent],
+      imports: [RouterTestingModule]
+    }).compileComponents();
+  });
 
-//   beforeEach(() => {
-//     TestBed.configureTestingModule({
-//       declarations: [BeforeStartComponent]
-//     });
-//     fixture = TestBed.createComponent(BeforeStartComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
+  beforeEach(() => {
+    fixture = TestBed.createComponent(BeforeStartComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
 
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
-// });
+  it('should create the component', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should have a constructor', () => {
+    expect(component.constructor).toBeDefined();
+  });
+
+  afterEach(() => {
+    fixture.destroy();
+  });
+});
