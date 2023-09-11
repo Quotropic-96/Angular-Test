a4fb0931dec3412f942b937fcaff417dbd748327
Gerard Solanes <gerardsolher@gmail.com>
2023-09-11 11:31:27 +0200

    Adds tests for icon component

--- src/app/components/icon/icon.component.spec.ts
+++ src/app/components/icon/icon.component.spec.ts
@@ -1,15 +1,17 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { IconComponent } from './icon.component';
 
 describe('IconComponent', () => {
   let component: IconComponent;
   let fixture: ComponentFixture<IconComponent>;
 
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [IconComponent],
+    }).compileComponents();
+  });
+
   beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [IconComponent]
-    });
     fixture = TestBed.createComponent(IconComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -17,5 +19,15 @@ describe('IconComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have empty iconName by default', () => {
+    expect(component.iconName).toBe('');
+  });
+
+  it('should reflect changes for iconName Input', () => {
+    component.iconName = 'example-icon';
+    fixture.detectChanges();
+    expect(component.iconName).toBe('example-icon');
   });
 });
