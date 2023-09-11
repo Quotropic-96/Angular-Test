4b44fd4fb504644ea88c6ae1f56a858bec5ce799
Gerard Solanes <gerardsolher@gmail.com>
2023-09-11 11:44:14 +0200

    Adds tests for navbar component

--- src/app/components/navbar/navbar.component.spec.ts
+++ src/app/components/navbar/navbar.component.spec.ts
@@ -1,21 +1,49 @@
-// import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { NavbarComponent } from './navbar.component';
+import { MenuService } from 'src/app/services/menu-service/menu.service';
+import { of } from 'rxjs';
+import { IconComponent } from '../icon/icon.component';
 
-// import { NavbarComponent } from './navbar.component';
+describe('NavbarComponent', () => {
+  let component: NavbarComponent;
+  let fixture: ComponentFixture<NavbarComponent>;
+  let mockMenuService: jasmine.SpyObj<MenuService>;
 
-// describe('NavbarComponent', () => {
-//   let component: NavbarComponent;
-//   let fixture: ComponentFixture<NavbarComponent>;
+  beforeEach(async () => {
+    mockMenuService = jasmine.createSpyObj('MenuService', [
+      'toggleMenu',
+      'isMenuOpen$',
+    ]);
 
-//   beforeEach(() => {
-//     TestBed.configureTestingModule({
-//       declarations: [NavbarComponent]
-//     });
-//     fixture = TestBed.createComponent(NavbarComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
+    await TestBed.configureTestingModule({
+      declarations: [NavbarComponent, IconComponent],
+      providers: [{ provide: MenuService, useValue: mockMenuService }],
+    }).compileComponents();
+  });
 
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
-// });
+  beforeEach(() => {
+    fixture = TestBed.createComponent(NavbarComponent);
+    component = fixture.componentInstance;
+    mockMenuService.isMenuOpen$ = of(false);
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should toggle the menu using menuService', () => {
+    component.toggleMenu();
+    expect(mockMenuService.toggleMenu).toHaveBeenCalled();
+  });
+
+  it('should get the menu open state from menuService', (done: DoneFn) => {
+    const testValue = true;
+    mockMenuService.isMenuOpen$ = of(testValue);
+
+    component.isMenuOpen().subscribe(result => {
+      expect(result).toBe(testValue);
+      done();
+    });
+  });
+});
